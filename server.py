2ac139cb20dd52850612a45d05718fae1bbb7e84
Nils Lopez <lopez.nils@codawalp.io>
2022-07-24 16:56:39 +0200

    Exit orders and MongoDb

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ from binance.client import Client
 from binance.um_futures import UMFutures
 from flask import Flask, request
 from dotenv import dotenv_values
-from datetime import datetime
 
 config = dotenv_values(".env")
 
@@ -16,9 +15,11 @@ client = Client(config['BINANCE_APIKEY'], config['BINANCE_SECRET'], testnet=Fals
 
 from storage import *
 
-now = datetime.now()
+from pymongo import MongoClient
 
-def order(side, symbol, order_price, passphrase, order_size, leverage, loss, profit):
+dbClient = MongoClient(config['MONGODB_URI'])
+
+def order(side, symbol, order_price, passphrase, order_size, leverage, loss, profit, req_type, strategy, exchange):
     try:
         balance = future_client.balance(recvWindow=6000)
 
@@ -26,20 +27,18 @@ def order(side, symbol, order_price, passphrase, order_size, leverage, loss, pro
             if a['asset'] == "USDT":
                 balance = a['availableBalance']
 
-        quantity =  round(float(float(balance)*(order_size*0.01)/float(order_price)), 3)
-        if leverage:
-            await client.futures_change_leverage(symbol=symbol, leverage=leverage)
-        longSl = round(float(order_price)*(1 - (float(loss)*0.01)), 2)
-        longTp = round(float(order_price)*(1 + (float(profit*0.01)), 2)
-        shortSl = round(float(order_price)*(1 + (float(loss)*0.01)), 2)
-        shortTp = round(float(order_price)*(1 - (float(profit)*0.01)), 2)
-        print(f"{passphrase} {symbol} Sending order - {side} {symbol} at {order_price}. quantity :  {quantity} and balance : {balance}")
-        order = client.futures_create_order(symbol=symbol, side=side, type="MARKET", quantity=quantity,
+        quantity =  round(float(float(balance)*(float(order_size)*0.01)/float(order_price)), 3)
+        client.futures_change_leverage(symbol=symbol, leverage=leverage)
+        print(f"{passphrase} trading {symbol} with {strategy} on {exchange} Sending order - {side} {symbol} at {order_price}. quantity :  {quantity} and balance : {balance}")
+        order = ""
+        if req_type == "order":
+            order = client.futures_create_order(symbol=symbol, side=side, type="MARKET", quantity=quantity,
                                                 isolated=True)
-        if order and loss != "exit":
             print(f" {passphrase} {symbol} Executed order {order}. {now}")
 
             if side == "BUY":
+                longSl = round(float(order_price) * (1 - (float(loss) * 0.01)), 2)
+                longTp = round(float(order_price) * (1 + (float(profit) * 0.01)), 2)
                 slorder = client.futures_create_order(symbol=symbol, side="SELL", type="STOP_MARKET",
                                                       quantity=quantity, stopPrice=longSl)
                 print(f" sl : {slorder}")
@@ -49,8 +48,10 @@ def order(side, symbol, order_price, passphrase, order_size, leverage, loss, pro
                                                       stopPrice=longTp)
                 print(f" tp : {tporder}")
                 if (slorder and tporder):
-                    saveOrder(now, slorder, tporder, order, config['GIT_TOKEN'])
+                    saveOrder(now, slorder, tporder, order, config['GIT_TOKEN'], quantity, exchange, strategy)
             elif side == "SELL":
+                shortSl = round(float(order_price) * (1 + (float(loss) * 0.01)), 2)
+                shortTp = round(float(order_price) * (1 - (float(profit) * 0.01)), 2)
                 print("tsb")
                 slorder = client.futures_create_order(symbol=symbol, side="BUY", type="STOP_MARKET",
                                       quantity=quantity, stopPrice=shortSl)
@@ -60,15 +61,17 @@ def order(side, symbol, order_price, passphrase, order_size, leverage, loss, pro
                                       stopPrice=shortTp)
                 print(f" tp : {tporder}")
                 if (slorder and tporder):
-                    saveOrder(now, slorder, tporder, order, config['GIT_TOKEN'])
+                    ord = saveOrder(dbClient, order, quantity, exchange, strategy, symbol)
+                    print(f"Successfully saved order in to Database : {ord}")
             else:
                 print("========================")
                 print("Sand request")
                 print("========================")
         else:
-            # order = client.futures_create_order(symbol=symbol, side=side, type="MARKET", quantity=quantity)
-            if order and loss == "exit":
-                print(f"Successfully stopped order : {order}")
+            if req_type == "exit":
+                savedOrder = getOrder(dbClient, exchange, strategy, symbol)
+                order = client.futures_create_order(symbol=symbol, side=side, type="MARKET", quantity=savedOrder)
+                print(f"Successfully stopped order : {order} {savedOrder}")
             else:
                 print(f" {passphrase} {symbol} Not Executed order - {side} {quantity} {symbol} at {order_price}.")
     except Exception as e:
@@ -101,7 +104,10 @@ def webhook():
     loss = data['stop_loss']
     profit = data['take_profit']
     order_size = data['order_size']
-    order_response = order(side, ticker, order_price, data['passphrase'], order_size, leverage, loss, profit)
+    req_type = data['req_type']
+    strategy = data['strategy']
+    exchange = data['exchange']
+    order_response = order(side, ticker, order_price, data['passphrase'], order_size, leverage, loss, profit, req_type, strategy, exchange)
 
 
     if order_response:
--- storage.py
+++ storage.py
@@ -1,15 +1,42 @@
-from github import Github
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+def connectDb ():
+    try:
+        db = client.admin
+        # Issue the serverStatus command and print the results
+        serverStatusResult = db.command("serverStatus")
+        print(serverStatusResult)
+    except Exception as e:
+        print("An exception occured {}".format(e))
 
 
-def saveOrder (time, slorder, tporder, order, token):
-    print(f'Saving order : {order}');
-    github = Github(token)
+def saveOrder (dbClient, order, quantity, exchange, strategy, symbol):
+    try:
+        db = dbClient.pyBot
+        data = {
+            "strategy": strategy,
+            "exchange": exchange,
+            "quantity": quantity,
+            "symbol": symbol,
+            "order": order,
+        }
+        res = db.orders.insert_one(data)
+        print(f'Saving order : {res}')
+    except Exception as e:
+        print('An exception occured : {}'.format(e))
 
-    repository = github.get_user().get_repo('ocTrader-data')
-    filename = f'orders/{time}.json'
+def getOrder (dbClient, exchange, strategy, symbol):
+    try:
+        db = dbClient.pyBot
+        strat_last_order = db.orders.find({"$and": [{"strategy": strategy}, {"exchange": exchange}, {"symbol": symbol}]}).limit(1).sort([('$natural',-1)])
 
-    content = (f'ORDER : {order}, STOP-LOSS: {slorder}, TAKEPROFIT: {tporder}')
+        print(f'Strat order last one : {strat_last_order}')
+        for x in strat_last_order :
+            quantity = x["quantity"]
+            if quantity:
+                return quantity
 
-    f = repository.create_file(filename, "new order from OctoPyBot using pyGithub", content)
-
-    print(f'repo : {repository}')
+    except Exception as e:
+        print ('An exception occure mgl : {}'.format(e))
